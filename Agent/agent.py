22a742b40de68ff21c16a2b99104b96ed8917673
EnormousDoggo <130384696+EnormousDoggo@users.noreply.github.com>
2024-05-25 13:42:26 +0200

    making json work

--- Agent/agent.py
+++ Agent/agent.py
@@ -1,39 +1,47 @@
-import requests, re, time
+# importations et variables
+import requests, re, time, sys
 
 PORT = 12345
 run = True
 
-# connect to the controller
-print("Entrez l'ip et port du contrôleur :") 
-ControllerIP = input("IP -> ")
-ControllerPort = input("Port -> ")
-print("Entrez le nom du nouvel agent :")
-AgentName = input("Nom -> ")
+arguments = sys.argv[1:]
+try:
+	ControllerIP = arguments[0]
+except:
+	print("Entrez l'ip du contrôleur :") 
+	ControllerIP = input("IP -> ")
+try:
+	ControllerPort = arguments[1]
+except:
+	print("Entrez le port du contrôleur :") 
+	ControllerPort = input("Port -> ")
+try:
+	AgentName = arguments[2]
+except:
+	print("Entrez le nom du nouvel agent :")
+	AgentName = input("Nom -> ")
 ControllerUrl = "http://"+ControllerIP+":"+ControllerPort
-# send the request
+# envoi de la requête
 response = requests.get(ControllerUrl+"/enroll/"+AgentName)
 if response.status_code == 200 :
 	print("Connexion avec le contrôleur établie")
 else :
 	print("Echec de la connexion")
+	run=False
 
-# ask the controller for instructions every 2 seconds
+# lancement du serveur
 while run == True :
     response = requests.get(ControllerUrl+"/instruction/"+AgentName)
     if response.text == "quit":
-        print("fin du programme")
         run = False
     if response.text == "whoami":
         # appeler le script 
-        # renvoyer le résultat au controleur
         requests.post(ControllerUrl+"/")
     if response.text == "list directories":
         # appeler le script 
-        # renvoyer le résultat au controleur
         requests.post(ControllerUrl+"/")
     if response.text == "drop file ":
         # appeler le script 
-        # renvoyer le résultat au controleur 
         requests.post(ControllerUrl+"/")
     time.sleep(2)
         
--- Controller/controller.py
+++ Controller/controller.py
@@ -13,11 +13,12 @@ def agents():
 
 @app.route('/enroll/<name>')
 def enroll(name):
-    newHost = {str(name):str(request.remote_addr)}
-    with open('agentsList.json', 'w') as hosts:
-        existing = json.load(hosts)
-        existing.append(json.dumps(newHost))
-        json.dump(existing, hosts)
+    with open('agentsList.json', 'r+') as agents:
+        existing = json.load(agents)
+        existing[name] = str(request.remote_addr)
+        agents.seek(0)
+        json.dump(existing, agents)
+        agents.truncate()
     instructionsDic[name]=""
     return '', 200
 
